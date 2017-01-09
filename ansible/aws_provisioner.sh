8e46cf1fe0aa75ee15440c16a1b07208ccc64414
davelush <dave.lush@gmail.com>
2017-01-09 20:58:55 +0000

    Removed aws_provisioner.sh and replaced with a few commands documented in a README.md. Also renamed the tag in ec2_launch.yml

--- ansible/aws_provisioner.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# I'll keep this here because I'm lazy and I don't want it in my bash profile
-
-export AWS_ACCESS_KEY_ID='add yours here'
-export AWS_SECRET_ACCESS_KEY='add yours here'
