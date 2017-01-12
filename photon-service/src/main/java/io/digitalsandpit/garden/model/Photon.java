a695bd040bce70c9b89e4d233e1b241e95cfbaae
davelush <dave.lush@gmail.com>
2017-01-12 13:06:04 +0000

    Starter for a Photon representation of a Device

---
+++ photon-service/src/main/java/io/digitalsandpit/garden/model/Photon.java
@@ -0,0 +1,11 @@
+package io.digitalsandpit.garden.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Photon implements Device {
+
+    public void addBatteryReading(Battery battery) {
+
+    }
+}
