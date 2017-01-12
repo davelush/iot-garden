ca9ab7fb87a127a6c62116fbbd9fd32da7a62841
davelush <dave.lush@gmail.com>
2017-01-12 13:02:43 +0000

    Refactored packages from org to io prefix

---
+++ photon-service/src/main/java/io/digitalsandpit/garden/model/Device.java
@@ -0,0 +1,9 @@
+package io.digitalsandpit.garden.model;
+
+import io.digitalsandpit.garden.model.Battery;
+
+public interface Device {
+
+    public void addBatteryReading(Battery battery);
+
+}
--- photon-service/src/main/java/org/digitalsandpit/garden/services/Device.java
+++
@@ -1,9 +0,0 @@
-package org.digitalsandpit.garden.services;
-
-import org.digitalsandpit.garden.model.Battery;
-
-public interface Device {
-
-    public void addBatteryReading(Battery battery);
-
-}
